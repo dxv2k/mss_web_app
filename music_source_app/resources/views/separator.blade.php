efc1b9d945d6658f1d7e652e46fbcf99c386b688
dxv2k <9569076+dxv2k@users.noreply.github.com>
2021-06-19 01:20:23 +0700

    complete basic left side (Music Separator) for upload

--- music_source_app/resources/views/separator.blade.php
+++ music_source_app/resources/views/separator.blade.php
@@ -5,44 +5,96 @@
 
 <body class="bg-dark">
     <div class="container">
+        <!-- Left side card  -->
         <div class="row">
             <div class="col-lg-6 mb-4">
-                <div class="card">
+                <div class="card card-bg">
                     <img class="card-img-top" src="" alt="">
-
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">
+                        <!-- title -->
+                        <center>
+                            <h5 class="card-title separator-title">Music Separator</h5>
+                        </center>
+
+                        <!-- upload file -->
+                        <form>
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input" id="customFile">
+                                <label class="custom-file-label" for="customFile">Choose file</label>
+                            </div>
+                        </form>
+
+                        <script>
+                        // Add the following code if you want the name of the file appear on select
+                        $(".custom-file-input").on("change", function() {
+                            var fileName = $(this).val().split("\\").pop();
+                            $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+                        });
+                        </script>
+
+                        <!-- BUTTON  -->
+                        <!-- <div class="btn-group btn-group-toggle" data-toggle="buttons"> -->
+                        <div class="btn-group-toggle " data-toggle="buttons">
+                            <label class="btn btn-secondary active">
+                                <input type="radio" name="options" id="option1" autocomplete="off" checked>
+                                2 stems: Vocals (singing voice)/ Accompaniment separation
+                            </label>
+                            <br>
+                            <label class="btn btn-secondary">
+                                <input type="radio" name="options" id="option2" autocomplete="off">
+                                4 stems: Vocals / Drums / Bass / Other separation
+                            </label>
+                            <br>
+                            <label class="btn btn-secondary">
+                                <input type="radio" name="options" id="option3" autocomplete="off">
+                                5 stems: Vocals / Drums / Bass / Piano / Other separation
+                            </label>
+                        </div>
+                        <!-- <p class="card-text">
                             Some quick example text to build on
                             the card title and make up the bulk
                             of the card's content.
-                        </p>
-
-                        <a href="#" class="btn btn-outline-primary btn-sm">
-                            Card link
-                        </a>
-                        <a href="#" class="btn btn-outline-secondary btn-sm">
-                            <i class="far fa-heart"></i></a>
+                        </p> -->
+                        <!-- Button  -->
+                        <!-- <a href="#" class="btn btn-outline-primary btn-sm"> -->
+                        <br> <br>
+                        <center>
+                            <a href="#" class="btn btn-danger btn-lg">
+                                Separate!
+                            </a>
+                        </center>
                     </div>
                 </div>
             </div>
+            <!-- Right side card  -->
             <div class="col-lg-6 mb-4">
-                <div class="card">
+                <div class="card card-bg">
                     <img class="card-img-top" src="" alt="">
 
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">
+                        <h5 class="card-title separator-title">Playback</h5>
+                        <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                            <label class="btn btn-secondary active">
+                                <input type="radio" name="options" id="option1" autocomplete="off" checked> Active
+                            </label>
+                            <label class="btn btn-secondary">
+                                <input type="radio" name="options" id="option2" autocomplete="off"> Radio
+                            </label>
+                            <label class="btn btn-secondary">
+                                <input type="radio" name="options" id="option3" autocomplete="off"> Radio
+                            </label>
+                        </div>
+                        <br>
+                        <!-- <p class="card-text">
                             Some quick example text to build on the
                             card title and make up the bulk of the
                             card's content.
-                        </p>
-
-                        <a href="#" class="btn btn-outline-primary btn-sm">
+                        </p> -->
+                        <!-- Button  -->
+                        <!-- <a href="#" class="btn btn-outline-primary btn-sm"> -->
+                        <a href="#" class="btn btn-danger">
                             Card link
                         </a>
-                        <a href="#" class="btn btn-outline-secondary btn-sm">
-                            <i class="far fa-heart"></i></a>
                     </div>
                 </div>
             </div>
