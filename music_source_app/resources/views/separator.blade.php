d3f07329761e83d8846fd3edc4ff4512fa937bfd
dxv2k <9569076+dxv2k@users.noreply.github.com>
2021-06-18 17:16:05 +0700

    init separator page

---
+++ music_source_app/resources/views/separator.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+<h1>Separator page</h1>
+
+@endsection
--- music_source_app/routes/web.php
+++ music_source_app/routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/separator',)->name('separator');
+
+Route::get('/separator', function(){ 
+    return view('separator'); 
+}); 
